08b4fc656f335313efc04560e9f4a90ea5d0b158
TimMaxw <TimMaxw@1754e6c4-832e-0410-bb55-0fb906f63d99>
2009-03-21 20:45:17 +0000

    Improved testing module slightly and added two more tests.

---
+++ trunk/test/simple_macros.py
@@ -0,0 +1,19 @@
+import ctypesgentest
+from ctypes import *
+
+header = """
+#define A 1
+#define B(x,y) x+y
+#define C(a,b,c) a?b:c
+#define funny(x) "funny" #x
+"""
+
+module, output = ctypesgentest.test(header)
+
+assert module.A==1
+assert module.B(2,2)==4
+assert module.C(True, 1,2)==1
+assert module.C(False, 1,2)==2
+assert module.funny("bunny") == "funnybunny"
+
+print "Tests OK."
