3626bd8549ba5cda087c36831cd5e4af6d0d7e37
clach04 <clach04@1754e6c4-832e-0410-bb55-0fb906f63d99>
2010-09-30 01:00:36 +0000

    Fix issue #10, remove obsolete wrap.py

--- wrap.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# The name wrap.py is obsolete - the main file for ctypesgen is now
-# ctypesgen.py. This wrapper exists only to pass its arguments
-# ctypesgen.py
-
-import sys
-execfile(sys.argv[0].replace("wrap.py","ctypesgen.py"))
