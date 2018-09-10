d6e629e78255444b81ca95d8b33827b356196781
Spencer E. Olson <olsonse@umich.edu>
2018-09-10 11:41:23 -0600

    add debian package configuration
    Build the debian package with:
    fakeroot debian/rules binary

--- setup.py
+++ setup.py
@@ -8,14 +8,41 @@
 
 """
 
+import os
 from setuptools import setup, find_packages
+
+THIS_DIR = os.path.dirname( __file__ )
 
 import ctypesgencore
 
-setup(name='ctypesgen',
-    version=ctypesgencore.VERSION,
-    description='Python wrapper generator for ctypes',
-    url='http://code.google.com/p/ctypesgen/',
-    license='BSD License',
-    packages=find_packages(),
-    scripts=['ctypesgen.py'])
+ctypesgencore.version.write_version_file()
+
+VERSION_FILE = os.path.relpath(ctypesgencore.version.VERSION_FILE, THIS_DIR)
+f = open('MANIFEST.in', 'w')
+f.write('include {}\n'.format(VERSION_FILE))
+f.write('graft ctypesgencore\n')
+f.close()
+
+try:
+    setup(
+        name='ctypesgen',
+        version=ctypesgencore.VERSION.partition('-g')[0],
+        description='Python wrapper generator for ctypes',
+        long_description=
+            'ctypesgen reads parses c header files and creates a wrapper for '
+            'libraries base on what it finds.  Preprocessor macros are handled '
+            'in a manner consistent with typical c code.  Preprocessor macro '
+            'functions are translated into Python functions that are then made '
+            'available to the user of the newly-generated Python wrapper '
+            'library.\n'
+            'ctypesgen can also output JSON, which can be used with Mork, '
+            'which generates bindings for Lua, using the alien module (which '
+            'binds libffi to Lua).',
+        url='https://github.com/olsonse/ctypesgen',
+        license='BSD License',
+        packages=find_packages(),
+        include_package_data=True,
+        scripts=['ctypesgen.py'],
+    )
+finally:
+    os.unlink('MANIFEST.in')
