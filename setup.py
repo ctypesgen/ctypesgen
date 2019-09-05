166332ec8a621ddffe4039e58de07cb4f68d6d88
Alan Robertson <alanr@unix.sh>
2019-09-05 06:40:07 -0600

    Improve spelling/meta-info in setup.py
    Adds information to setup.py to make a warning go away and also give more
    package information.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ try:
         version=ctypesgen.VERSION.partition("-g")[0],
         description="Python wrapper generator for ctypes",
         long_description="ctypesgen reads parses c header files and creates a wrapper for "
-        "libraries base on what it finds.  Preprocessor macros are handled "
+        "libraries based on what it finds.  Preprocessor macros are handled "
         "in a manner consistent with typical c code.  Preprocessor macro "
         "functions are translated into Python functions that are then made "
         "available to the user of the newly-generated Python wrapper "
@@ -38,10 +38,22 @@ try:
         "ctypesgen can also output JSON, which can be used with Mork, "
         "which generates bindings for Lua, using the alien module (which "
         "binds libffi to Lua).",
+        long_description_content_type="text/plain",
         url="https://github.com/davidjamesca/ctypesgen",
         license="BSD License",
         packages=find_packages(),
         include_package_data=True,
+        classifiers=[
+            "License :: OSI Approved :: BSD License",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Development Status :: 4 - Beta",
+            "Operating System :: OS Independent",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Code Generators",
+            "Topic :: Software Development :: Pre-processors",
+            "Topic :: Software Development :: Build Tools",
+        ],
         entry_points={"console_scripts": ["ctypesgen = ctypesgen.main:main"]},
     )
 finally:
