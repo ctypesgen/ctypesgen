ctypesgen/ctypesgen

commit d5ef38c5d8efb7d05db4d998e028efadf6864dbf

Allow muitiple -i/--include-symbols -x/--exclude-symbols options
Closes davidjamesca/ctypesgen#31

ctypesgen/processor/operations.py
@@ -69,7 +69,7 @@ def filter_by_regexes_exclude(data, opts):
     """filter_by_regexes_exclude() uses regular expressions specified by options
     dictionary to filter symbols."""
     if opts.exclude_symbols:
-        expr = re.compile(opts.exclude_symbols)
+        expr = re.compile('({})'.format('|'.join(opts.exclude_symbols)))
         for object in data.all:
             if expr.match(object.py_name()):
                 object.include_rule = "never"
@@ -79,7 +79,7 @@ def filter_by_regexes_include(data, opts):
     """filter_by_regexes_include() uses regular expressions specified by options
     dictionary to re-include symbols previously rejected by other operations."""
     if opts.include_symbols:
-        expr = re.compile(opts.include_symbols)
+        expr = re.compile('({})'.format('|'.join(opts.include_symbols)))
         for object in data.all:
             if object.include_rule != "never":
                 if expr.match(object.py_name()):

ctypesgen/main.py
@@ -187,16 +187,24 @@ def main(givenargs=None):
     op.add_option(
         "-i",
         "--include-symbols",
+        action="append",
         dest="include_symbols",
-        default=None,
-        help="regular expression for symbols to always include",
+        metavar="REGEXPR",
+        default=[],
+        help="Regular expression for symbols to always include.  Multiple "
+        "instances of this option will be combined into a single expression "
+        "doing something like '(expr1|expr2|expr3)'.",
     )
     op.add_option(
         "-x",
         "--exclude-symbols",
+        action="append",
         dest="exclude_symbols",
-        default=None,
-        help="regular expression for symbols to exclude",
+        metavar="REGEXPR",
+        default=[],
+        help="Regular expression for symbols to exclude.  Multiple instances "
+        "of this option will be combined into a single expression doing "
+        "something like '(expr1|expr2|expr3)'.",
     )
     op.add_option(
         "",

ctypesgen/options.py
@@ -20,8 +20,8 @@ default_values = {
     "save_preprocessed_headers": None,
     "all_headers": False,
     "builtin_symbols": False,
-    "include_symbols": None,
-    "exclude_symbols": None,
+    "include_symbols": [],
+    "exclude_symbols": [],
     "show_all_errors": False,
     "show_long_errors": False,
     "show_macro_warnings": True,

commit a451300a6a0d4dfba5afe6c869bcb3aeaa7b0a2a

Fixes "black" formatting introduced in d5ef38c5d8e
Fixes: d5ef38c5d8e

ctypesgen/processor/operations.py
@@ -69,7 +69,7 @@ def filter_by_regexes_exclude(data, opts):
     """filter_by_regexes_exclude() uses regular expressions specified by options
     dictionary to filter symbols."""
     if opts.exclude_symbols:
-        expr = re.compile('({})'.format('|'.join(opts.exclude_symbols)))
+        expr = re.compile("({})".format("|".join(opts.exclude_symbols)))
         for object in data.all:
             if expr.match(object.py_name()):
                 object.include_rule = "never"
@@ -79,7 +79,7 @@ def filter_by_regexes_include(data, opts):
     """filter_by_regexes_include() uses regular expressions specified by options
     dictionary to re-include symbols previously rejected by other operations."""
     if opts.include_symbols:
-        expr = re.compile('({})'.format('|'.join(opts.include_symbols)))
+        expr = re.compile("({})".format("|".join(opts.include_symbols)))
         for object in data.all:
             if object.include_rule != "never":
                 if expr.match(object.py_name()):