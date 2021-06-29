b4c6d13de447f408393e2dfd02e63a9079a6314d
KOLANICH <kolan_n@mail.ru>
2021-06-29 13:27:35 +0300

    Moved metadata into setup.cfg Got rid of own fetching version from git tags in favour of setuptools_scm Added pyproject.toml Persisted MANIFEST.in into the repo

--- setup.py
+++ setup.py
@@ -8,53 +8,7 @@
 
 """
 
-import os
 from setuptools import setup, find_packages
 
-THIS_DIR = os.path.dirname(__file__)
-
-import ctypesgen
-
-ctypesgen.version.write_version_file()
-
-VERSION_FILE = os.path.relpath(ctypesgen.version.VERSION_FILE, THIS_DIR)
-f = open("MANIFEST.in", "w")
-f.write("include {}\n".format(VERSION_FILE))
-f.write("graft ctypesgen\n")
-f.write("recursive-exclude ctypesgen .gitignore\n")
-f.close()
-
-try:
-    setup(
-        name="ctypesgen",
-        version=ctypesgen.VERSION.partition("-g")[0],
-        description="Python wrapper generator for ctypes",
-        long_description="ctypesgen reads parses c header files and creates a wrapper for "
-        "libraries based on what it finds.  Preprocessor macros are handled "
-        "in a manner consistent with typical c code.  Preprocessor macro "
-        "functions are translated into Python functions that are then made "
-        "available to the user of the newly-generated Python wrapper "
-        "library.\n"
-        "ctypesgen can also output JSON, which can be used with Mork, "
-        "which generates bindings for Lua, using the alien module (which "
-        "binds libffi to Lua).",
-        long_description_content_type="text/plain",
-        url="https://github.com/davidjamesca/ctypesgen",
-        license="BSD License",
-        packages=find_packages(),
-        include_package_data=True,
-        classifiers=[
-            "License :: OSI Approved :: BSD License",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Development Status :: 4 - Beta",
-            "Operating System :: OS Independent",
-            "Intended Audience :: Developers",
-            "Topic :: Software Development :: Code Generators",
-            "Topic :: Software Development :: Pre-processors",
-            "Topic :: Software Development :: Build Tools",
-        ],
-        entry_points={"console_scripts": ["ctypesgen = ctypesgen.main:main"]},
-    )
-finally:
-    os.unlink("MANIFEST.in")
+if __name__ == "__main__":
+    setup()
