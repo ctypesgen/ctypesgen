ff0b201dc68f6f66747045536e82e294d8c9657a
geisserml <geisserml@gmail.com>
2022-11-26 17:56:26 +0100

    make _get_ptrdiff_t() slightly nicer

--- ctypesgen/printer_python/preamble.py
+++ ctypesgen/printer_python/preamble.py
@@ -3,15 +3,19 @@ from ctypes import *  # noqa: F401, F403
 
 
 def _get_ptrdiff_t():
+
     int_types = (ctypes.c_int16, ctypes.c_int32)
     if hasattr(ctypes, "c_int64"):
         # Some builds of ctypes apparently do not have ctypes.c_int64
         # defined; it's a pretty good bet that these builds do not
         # have 64-bit pointers.
         int_types += (ctypes.c_int64,)
+
+    c_ptrdiff_t = None
     for t in int_types:
         if ctypes.sizeof(t) == ctypes.sizeof(ctypes.c_size_t):
             c_ptrdiff_t = t
+
     return c_ptrdiff_t
 
 
