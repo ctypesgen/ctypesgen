9c4f61a2169ee92ee0b397c30d6a03bd9576be01
geisserml <geisserml@gmail.com>
2023-06-19 12:44:06 +0200

    Fix 101 line (flake8)

--- ctypesgen/printer_python/printer.py
+++ ctypesgen/printer_python/printer.py
@@ -302,8 +302,8 @@ class WrapperPrinter:
 
         CC = "stdcall" if function.attrib.get("stdcall", False) else "cdecl"
 
-        # If we know what library the function lives in or we have only a single library, look there.
-        # Otherwise, check all the libraries.
+        # If we know what library the function lives in, or we have only a single library,
+        # look there. Otherwise, check all the libraries.
         use_single_lib = function.source_library or len(self.options.libraries) == 1
         if use_single_lib:
             lib = function.source_library if function.source_library else self.options.libraries[0]
